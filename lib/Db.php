a57094eb57107b211486d131ecc047811f21210c
tomheng <zhm20070928@gmail.com>
2016-07-07 11:49:55 +0800

    add cache key to static var

--- lib/Db.php
+++ lib/Db.php
@@ -3,11 +3,12 @@ class Db {
 
 	//初始化一个mysql实例
 	public static function mysql($config, $dbname = '') {
-		static $mysql_obj = null;
-		if ($mysql_obj == null) {
-			$mysql_obj = new MysqlDb($config, $dbname);
+		static $mysql_objs = array();
+                $key = md5(json_encode(func_get_args()));
+		if (!isset($mysql_objs[$key])) {
+			$mysql_objs[$key] = new MysqlDb($config, $dbname);
 		}
-		return $mysql_obj;
+		return $mysql_objs[$key];
 	}
 
 }
