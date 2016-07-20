7f8fbf2b9b4b52ac11fd586deb1fd05c2070eb33
tomheng <zhm20070928@gmail.com>
2016-07-20 10:57:36 +0800

    format

--- lib/Db.php
+++ lib/Db.php
@@ -4,7 +4,7 @@ class Db {
 	//初始化一个mysql实例
 	public static function mysql($config, $dbname = '') {
 		static $mysql_objs = array();
-                $key = md5(json_encode(func_get_args()));
+		$key               = md5(json_encode(func_get_args()));
 		if (!isset($mysql_objs[$key])) {
 			$mysql_objs[$key] = new MysqlDb($config, $dbname);
 		}
