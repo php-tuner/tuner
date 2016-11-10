4e155b71ea22af98280a3bafa2a7f1fc06124aaf
tomheng <zhm20070928@gmail.com>
2016-11-10 17:22:30 +0800

    set pdo default fetch mode

--- config/pdo.php
+++ config/pdo.php
@@ -2,9 +2,9 @@
 // Copyright 2016 The PHP Tuner Authors. All rights reserved.
 // Use of this source code is governed by a GPL-3.0
 // license that can be found in the LICENSE file.
-
 //PDO设置
 return array(
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 	PDO::ATTR_TIMEOUT => 3,
 );
