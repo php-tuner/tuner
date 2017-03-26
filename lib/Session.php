9de5b4dedcb24d2d484509ad1081af4ce805cfe9
tomheng <zhm20070928@gmail.com>
2017-03-26 19:15:28 +0800

    add mergeUpdate and setExpire function

--- lib/Session.php
+++ lib/Session.php
@@ -12,6 +12,16 @@ class Session {
 		}
 	}
 
+	// 更新 session
+	public static function mergeUpdate($key, $value){
+		self::start();
+		$old_value = $_SESSION[$key];
+		if(is_array($old_value) && is_array($value) && $old_value != $value){
+			$value = array_merge_recursive($old_value, $value);
+		}
+		return $_SESSION[$key] = $value;
+	}
+
 	// 设置session
 	public static function set($key, $value) {
 		self::start();
@@ -33,5 +43,15 @@ class Session {
 			return $val;
 		}
 		return null;
+	}
+	
+	// 设置session cookie 超时时间
+	// http://php.net/session_set_cookie_params comment by final dot wharf at gmail dot com 
+	public static function setExpire($lifetime = 0){
+		$params = session_get_cookie_params();
+	    session_set_cookie_params($lifetime, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+		//session_regenerate_id(true);
+		//setcookie(session_name(), session_id(), time() + $lifetime);
+		//throw new Exception('====='.print_r(session_get_cookie_params(), true), 2);
 	}
 }
