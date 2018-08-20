f884bec47c24d0d8eaf60113a9b24cd204657f75
tomheng <zhm20070928@gmail.com>
2018-08-20 15:13:41 +0800

    checkout scalar value

--- Model.php
+++ Model.php
@@ -204,6 +204,9 @@ class Model
         }
         $set_array = array();
         foreach ($data_array as $key => $value) {
+            if(is_scalar($value) == false){
+                throw new Exception("{$key} is not scalar.");
+            }
             $value       = $this->escape($value);
             $key         = $this->escape($key);
             $set_array[] = " `$key` = '{$value}' ";
