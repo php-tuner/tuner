6d324ef2b4638e7fc0f48777d0fdc4e05000df54
tomheng <zhm20070928@gmail.com>
2018-08-31 17:29:36 +0800

    format

--- config/pdo.php
+++ config/pdo.php
@@ -2,7 +2,7 @@
 // Copyright 2016 The PHP Tuner Authors. All rights reserved.
 // Use of this source code is governed by a GPL-3.0
 // license that can be found in the LICENSE file.
-//PDO设置
+// PDO设置
 return array(
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
