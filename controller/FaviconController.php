15d1aba5781e7047f66409dc448f172b292f73a4
tomheng <zhm20070928@gmail.com>
2018-09-14 12:38:42 +0800

    add favicon controller

---
+++ controller/FaviconController.php
@@ -0,0 +1,16 @@
+<?php
+// Copyright 2016 The PHP Tuner Authors. All rights reserved.
+// Use of this source code is governed by a GPL-3.0
+// license that can be found in the LICENSE file.
+
+// favicon.icon
+// TODO do more interesting things.
+class FaviconController extends Controller {
+	
+    public function index()
+    {
+		header("Content-Type: image/x-icon");
+        $data = 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=';
+        exit(base64_decode($data));
+	}
+}
